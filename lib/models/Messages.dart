4aca33baf680f6317f9cdb4320620e265d6b8f82
mo <modullah@gmail.com>
2022-04-28 19:04:52 -0500

    resolving merge conflicts

--- lib/models/Messages.dart
+++ lib/models/Messages.dart
@@ -19,8 +19,9 @@
 
 // ignore_for_file: public_member_api_docs, file_names, unnecessary_new, prefer_if_null_operators, prefer_const_constructors, slash_for_doc_comments, annotate_overrides, non_constant_identifier_names, unnecessary_string_interpolations, prefer_adjacent_string_concatenation, unnecessary_const, dead_code
 
-import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_core/amplify_core.dart';
 import 'package:flutter/foundation.dart';
+
 
 /** This is an auto generated class representing the Messages type in your schema. */
 @immutable
@@ -41,132 +42,99 @@ class Messages extends Model {
 
   @override
   getInstanceType() => classType;
-
+  
   @override
   String getId() {
     return id;
   }
-
+  
   String? get sale {
     return _sale;
   }
-
+  
   String? get host {
     return _host;
   }
-
+  
   String? get customer {
     return _customer;
   }
-
+  
   String? get sender {
     return _sender;
   }
-
+  
   String? get receiver {
     return _receiver;
   }
-
+  
   bool? get senderSeen {
     return _senderSeen;
   }
-
+  
   bool? get receiverSeen {
     return _receiverSeen;
   }
-
+  
   String? get text {
     return _text;
   }
-
+  
   TemporalDateTime? get date {
     return _date;
   }
-
+  
   TemporalDateTime? get createdAt {
     return _createdAt;
   }
-
+  
   TemporalDateTime? get updatedAt {
     return _updatedAt;
   }
-
-  const Messages._internal(
-      {required this.id,
-      sale,
-      host,
-      customer,
-      sender,
-      receiver,
-      senderSeen,
-      receiverSeen,
-      text,
-      date,
-      createdAt,
-      updatedAt})
-      : _sale = sale,
-        _host = host,
-        _customer = customer,
-        _sender = sender,
-        _receiver = receiver,
-        _senderSeen = senderSeen,
-        _receiverSeen = receiverSeen,
-        _text = text,
-        _date = date,
-        _createdAt = createdAt,
-        _updatedAt = updatedAt;
-
-  factory Messages(
-      {String? id,
-      String? sale,
-      String? host,
-      String? customer,
-      String? sender,
-      String? receiver,
-      bool? senderSeen,
-      bool? receiverSeen,
-      String? text,
-      TemporalDateTime? date}) {
+  
+  const Messages._internal({required this.id, sale, host, customer, sender, receiver, senderSeen, receiverSeen, text, date, createdAt, updatedAt}): _sale = sale, _host = host, _customer = customer, _sender = sender, _receiver = receiver, _senderSeen = senderSeen, _receiverSeen = receiverSeen, _text = text, _date = date, _createdAt = createdAt, _updatedAt = updatedAt;
+  
+  factory Messages({String? id, String? sale, String? host, String? customer, String? sender, String? receiver, bool? senderSeen, bool? receiverSeen, String? text, TemporalDateTime? date}) {
     return Messages._internal(
-        id: id == null ? UUID.getUUID() : id,
-        sale: sale,
-        host: host,
-        customer: customer,
-        sender: sender,
-        receiver: receiver,
-        senderSeen: senderSeen,
-        receiverSeen: receiverSeen,
-        text: text,
-        date: date);
+      id: id == null ? UUID.getUUID() : id,
+      sale: sale,
+      host: host,
+      customer: customer,
+      sender: sender,
+      receiver: receiver,
+      senderSeen: senderSeen,
+      receiverSeen: receiverSeen,
+      text: text,
+      date: date);
   }
-
+  
   bool equals(Object other) {
     return this == other;
   }
-
+  
   @override
   bool operator ==(Object other) {
     if (identical(other, this)) return true;
     return other is Messages &&
-        id == other.id &&
-        _sale == other._sale &&
-        _host == other._host &&
-        _customer == other._customer &&
-        _sender == other._sender &&
-        _receiver == other._receiver &&
-        _senderSeen == other._senderSeen &&
-        _receiverSeen == other._receiverSeen &&
-        _text == other._text &&
-        _date == other._date;
+      id == other.id &&
+      _sale == other._sale &&
+      _host == other._host &&
+      _customer == other._customer &&
+      _sender == other._sender &&
+      _receiver == other._receiver &&
+      _senderSeen == other._senderSeen &&
+      _receiverSeen == other._receiverSeen &&
+      _text == other._text &&
+      _date == other._date;
   }
-
+  
   @override
   int get hashCode => toString().hashCode;
-
+  
   @override
   String toString() {
     var buffer = new StringBuffer();
-
+    
     buffer.write("Messages {");
     buffer.write("id=" + "$id" + ", ");
     buffer.write("sale=" + "$_sale" + ", ");
@@ -174,82 +142,48 @@ class Messages extends Model {
     buffer.write("customer=" + "$_customer" + ", ");
     buffer.write("sender=" + "$_sender" + ", ");
     buffer.write("receiver=" + "$_receiver" + ", ");
-    buffer.write("senderSeen=" +
-        (_senderSeen != null ? _senderSeen!.toString() : "null") +
-        ", ");
-    buffer.write("receiverSeen=" +
-        (_receiverSeen != null ? _receiverSeen!.toString() : "null") +
-        ", ");
+    buffer.write("senderSeen=" + (_senderSeen != null ? _senderSeen!.toString() : "null") + ", ");
+    buffer.write("receiverSeen=" + (_receiverSeen != null ? _receiverSeen!.toString() : "null") + ", ");
     buffer.write("text=" + "$_text" + ", ");
     buffer.write("date=" + (_date != null ? _date!.format() : "null") + ", ");
-    buffer.write("createdAt=" +
-        (_createdAt != null ? _createdAt!.format() : "null") +
-        ", ");
-    buffer.write(
-        "updatedAt=" + (_updatedAt != null ? _updatedAt!.format() : "null"));
+    buffer.write("createdAt=" + (_createdAt != null ? _createdAt!.format() : "null") + ", ");
+    buffer.write("updatedAt=" + (_updatedAt != null ? _updatedAt!.format() : "null"));
     buffer.write("}");
-
+    
     return buffer.toString();
   }
-
-  Messages copyWith(
-      {String? id,
-      String? sale,
-      String? host,
-      String? customer,
-      String? sender,
-      String? receiver,
-      bool? senderSeen,
-      bool? receiverSeen,
-      String? text,
-      TemporalDateTime? date}) {
+  
+  Messages copyWith({String? id, String? sale, String? host, String? customer, String? sender, String? receiver, bool? senderSeen, bool? receiverSeen, String? text, TemporalDateTime? date}) {
     return Messages._internal(
-        id: id ?? this.id,
-        sale: sale ?? this.sale,
-        host: host ?? this.host,
-        customer: customer ?? this.customer,
-        sender: sender ?? this.sender,
-        receiver: receiver ?? this.receiver,
-        senderSeen: senderSeen ?? this.senderSeen,
-        receiverSeen: receiverSeen ?? this.receiverSeen,
-        text: text ?? this.text,
-        date: date ?? this.date);
+      id: id ?? this.id,
+      sale: sale ?? this.sale,
+      host: host ?? this.host,
+      customer: customer ?? this.customer,
+      sender: sender ?? this.sender,
+      receiver: receiver ?? this.receiver,
+      senderSeen: senderSeen ?? this.senderSeen,
+      receiverSeen: receiverSeen ?? this.receiverSeen,
+      text: text ?? this.text,
+      date: date ?? this.date);
   }
-
-  Messages.fromJson(Map<String, dynamic> json)
-      : id = json['id'],
-        _sale = json['sale'],
-        _host = json['host'],
-        _customer = json['customer'],
-        _sender = json['sender'],
-        _receiver = json['receiver'],
-        _senderSeen = json['senderSeen'],
-        _receiverSeen = json['receiverSeen'],
-        _text = json['text'],
-        _date = json['date'] != null
-            ? TemporalDateTime.fromString(json['date'])
-            : null,
-        _createdAt = json['createdAt'] != null
-            ? TemporalDateTime.fromString(json['createdAt'])
-            : null,
-        _updatedAt = json['updatedAt'] != null
-            ? TemporalDateTime.fromString(json['updatedAt'])
-            : null;
-
+  
+  Messages.fromJson(Map<String, dynamic> json)  
+    : id = json['id'],
+      _sale = json['sale'],
+      _host = json['host'],
+      _customer = json['customer'],
+      _sender = json['sender'],
+      _receiver = json['receiver'],
+      _senderSeen = json['senderSeen'],
+      _receiverSeen = json['receiverSeen'],
+      _text = json['text'],
+      _date = json['date'] != null ? TemporalDateTime.fromString(json['date']) : null,
+      _createdAt = json['createdAt'] != null ? TemporalDateTime.fromString(json['createdAt']) : null,
+      _updatedAt = json['updatedAt'] != null ? TemporalDateTime.fromString(json['updatedAt']) : null;
+  
   Map<String, dynamic> toJson() => {
-        'id': id,
-        'sale': _sale,
-        'host': _host,
-        'customer': _customer,
-        'sender': _sender,
-        'receiver': _receiver,
-        'senderSeen': _senderSeen,
-        'receiverSeen': _receiverSeen,
-        'text': _text,
-        'date': _date?.format(),
-        'createdAt': _createdAt?.format(),
-        'updatedAt': _updatedAt?.format()
-      };
+    'id': id, 'sale': _sale, 'host': _host, 'customer': _customer, 'sender': _sender, 'receiver': _receiver, 'senderSeen': _senderSeen, 'receiverSeen': _receiverSeen, 'text': _text, 'date': _date?.format(), 'createdAt': _createdAt?.format(), 'updatedAt': _updatedAt?.format()
+  };
 
   static final QueryField ID = QueryField(fieldName: "messages.id");
   static final QueryField SALE = QueryField(fieldName: "sale");
@@ -261,84 +195,96 @@ class Messages extends Model {
   static final QueryField RECEIVERSEEN = QueryField(fieldName: "receiverSeen");
   static final QueryField TEXT = QueryField(fieldName: "text");
   static final QueryField DATE = QueryField(fieldName: "date");
-  static var schema =
-      Model.defineSchema(define: (ModelSchemaDefinition modelSchemaDefinition) {
+  static var schema = Model.defineSchema(define: (ModelSchemaDefinition modelSchemaDefinition) {
     modelSchemaDefinition.name = "Messages";
     modelSchemaDefinition.pluralName = "Messages";
-
+    
     modelSchemaDefinition.authRules = [
-      AuthRule(authStrategy: AuthStrategy.PUBLIC, operations: [
-        ModelOperation.CREATE,
-        ModelOperation.UPDATE,
-        ModelOperation.DELETE,
-        ModelOperation.READ
-      ])
+      AuthRule(
+        authStrategy: AuthStrategy.PUBLIC,
+        operations: [
+          ModelOperation.CREATE,
+          ModelOperation.UPDATE,
+          ModelOperation.DELETE,
+          ModelOperation.READ
+        ])
     ];
-
+    
     modelSchemaDefinition.addField(ModelFieldDefinition.id());
-
+    
     modelSchemaDefinition.addField(ModelFieldDefinition.field(
-        key: Messages.SALE,
-        isRequired: false,
-        ofType: ModelFieldType(ModelFieldTypeEnum.string)));
-
+      key: Messages.SALE,
+      isRequired: false,
+      ofType: ModelFieldType(ModelFieldTypeEnum.string)
+    ));
+    
     modelSchemaDefinition.addField(ModelFieldDefinition.field(
-        key: Messages.HOST,
-        isRequired: false,
-        ofType: ModelFieldType(ModelFieldTypeEnum.string)));
-
+      key: Messages.HOST,
+      isRequired: false,
+      ofType: ModelFieldType(ModelFieldTypeEnum.string)
+    ));
+    
     modelSchemaDefinition.addField(ModelFieldDefinition.field(
-        key: Messages.CUSTOMER,
-        isRequired: false,
-        ofType: ModelFieldType(ModelFieldTypeEnum.string)));
-
+      key: Messages.CUSTOMER,
+      isRequired: false,
+      ofType: ModelFieldType(ModelFieldTypeEnum.string)
+    ));
+    
     modelSchemaDefinition.addField(ModelFieldDefinition.field(
-        key: Messages.SENDER,
-        isRequired: false,
-        ofType: ModelFieldType(ModelFieldTypeEnum.string)));
-
+      key: Messages.SENDER,
+      isRequired: false,
+      ofType: ModelFieldType(ModelFieldTypeEnum.string)
+    ));
+    
     modelSchemaDefinition.addField(ModelFieldDefinition.field(
-        key: Messages.RECEIVER,
-        isRequired: false,
-        ofType: ModelFieldType(ModelFieldTypeEnum.string)));
-
+      key: Messages.RECEIVER,
+      isRequired: false,
+      ofType: ModelFieldType(ModelFieldTypeEnum.string)
+    ));
+    
     modelSchemaDefinition.addField(ModelFieldDefinition.field(
-        key: Messages.SENDERSEEN,
-        isRequired: false,
-        ofType: ModelFieldType(ModelFieldTypeEnum.bool)));
-
+      key: Messages.SENDERSEEN,
+      isRequired: false,
+      ofType: ModelFieldType(ModelFieldTypeEnum.bool)
+    ));
+    
     modelSchemaDefinition.addField(ModelFieldDefinition.field(
-        key: Messages.RECEIVERSEEN,
-        isRequired: false,
-        ofType: ModelFieldType(ModelFieldTypeEnum.bool)));
-
+      key: Messages.RECEIVERSEEN,
+      isRequired: false,
+      ofType: ModelFieldType(ModelFieldTypeEnum.bool)
+    ));
+    
     modelSchemaDefinition.addField(ModelFieldDefinition.field(
-        key: Messages.TEXT,
-        isRequired: false,
-        ofType: ModelFieldType(ModelFieldTypeEnum.string)));
-
+      key: Messages.TEXT,
+      isRequired: false,
+      ofType: ModelFieldType(ModelFieldTypeEnum.string)
+    ));
+    
     modelSchemaDefinition.addField(ModelFieldDefinition.field(
-        key: Messages.DATE,
-        isRequired: false,
-        ofType: ModelFieldType(ModelFieldTypeEnum.dateTime)));
-
+      key: Messages.DATE,
+      isRequired: false,
+      ofType: ModelFieldType(ModelFieldTypeEnum.dateTime)
+    ));
+    
     modelSchemaDefinition.addField(ModelFieldDefinition.nonQueryField(
-        fieldName: 'createdAt',
-        isRequired: false,
-        isReadOnly: true,
-        ofType: ModelFieldType(ModelFieldTypeEnum.dateTime)));
-
+      fieldName: 'createdAt',
+      isRequired: false,
+      isReadOnly: true,
+      ofType: ModelFieldType(ModelFieldTypeEnum.dateTime)
+    ));
+    
     modelSchemaDefinition.addField(ModelFieldDefinition.nonQueryField(
-        fieldName: 'updatedAt',
-        isRequired: false,
-        isReadOnly: true,
-        ofType: ModelFieldType(ModelFieldTypeEnum.dateTime)));
+      fieldName: 'updatedAt',
+      isRequired: false,
+      isReadOnly: true,
+      ofType: ModelFieldType(ModelFieldTypeEnum.dateTime)
+    ));
   });
 }
 
 class _MessagesModelType extends ModelType<Messages> {
   const _MessagesModelType();
-
+  
   @override
   Messages fromJson(Map<String, dynamic> jsonData) {
     return Messages.fromJson(jsonData);
--- lib/models/Sale.dart
+++ lib/models/Sale.dart
@@ -19,10 +19,11 @@
 
 // ignore_for_file: public_member_api_docs, file_names, unnecessary_new, prefer_if_null_operators, prefer_const_constructors, slash_for_doc_comments, annotate_overrides, non_constant_identifier_names, unnecessary_string_interpolations, prefer_adjacent_string_concatenation, unnecessary_const, dead_code
 
-import 'model_provider.dart';
-import 'package:amplify_flutter/amplify_flutter.dart';
+import 'ModelProvider.dart';
+import 'package:amplify_core/amplify_core.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/foundation.dart';
+
 
 /** This is an auto generated class representing the Sale type in your schema. */
 @immutable
@@ -40,107 +41,81 @@ class Sale extends Model {
 
   @override
   getInstanceType() => classType;
-
+  
   @override
   String getId() {
     return id;
   }
-
+  
   String? get title {
     return _title;
   }
-
+  
   String? get description {
     return _description;
   }
-
+  
   String? get condition {
     return _condition;
   }
-
+  
   String? get zipcode {
     return _zipcode;
   }
-
+  
   String? get price {
     return _price;
   }
-
+  
   List<SaleImage>? get SaleImages {
     return _SaleImages;
   }
-
+  
   TemporalDateTime? get createdAt {
     return _createdAt;
   }
-
+  
   TemporalDateTime? get updatedAt {
     return _updatedAt;
   }
-
-  const Sale._internal(
-      {required this.id,
-      title,
-      description,
-      condition,
-      zipcode,
-      price,
-      SaleImages,
-      createdAt,
-      updatedAt})
-      : _title = title,
-        _description = description,
-        _condition = condition,
-        _zipcode = zipcode,
-        _price = price,
-        _SaleImages = SaleImages,
-        _createdAt = createdAt,
-        _updatedAt = updatedAt;
-
-  factory Sale(
-      {String? id,
-      String? title,
-      String? description,
-      String? condition,
-      String? zipcode,
-      String? price,
-      List<SaleImage>? SaleImages}) {
+  
+  const Sale._internal({required this.id, title, description, condition, zipcode, price, SaleImages, createdAt, updatedAt}): _title = title, _description = description, _condition = condition, _zipcode = zipcode, _price = price, _SaleImages = SaleImages, _createdAt = createdAt, _updatedAt = updatedAt;
+  
+  factory Sale({String? id, String? title, String? description, String? condition, String? zipcode, String? price, List<SaleImage>? SaleImages}) {
     return Sale._internal(
-        id: id == null ? UUID.getUUID() : id,
-        title: title,
-        description: description,
-        condition: condition,
-        zipcode: zipcode,
-        price: price,
-        SaleImages: SaleImages != null
-            ? List<SaleImage>.unmodifiable(SaleImages)
-            : SaleImages);
+      id: id == null ? UUID.getUUID() : id,
+      title: title,
+      description: description,
+      condition: condition,
+      zipcode: zipcode,
+      price: price,
+      SaleImages: SaleImages != null ? List<SaleImage>.unmodifiable(SaleImages) : SaleImages);
   }
-
+  
   bool equals(Object other) {
     return this == other;
   }
-
+  
   @override
   bool operator ==(Object other) {
     if (identical(other, this)) return true;
     return other is Sale &&
-        id == other.id &&
-        _title == other._title &&
-        _description == other._description &&
-        _condition == other._condition &&
-        _zipcode == other._zipcode &&
-        _price == other._price &&
-        DeepCollectionEquality().equals(_SaleImages, other._SaleImages);
+      id == other.id &&
+      _title == other._title &&
+      _description == other._description &&
+      _condition == other._condition &&
+      _zipcode == other._zipcode &&
+      _price == other._price &&
+      DeepCollectionEquality().equals(_SaleImages, other._SaleImages);
   }
-
+  
   @override
   int get hashCode => toString().hashCode;
-
+  
   @override
   String toString() {
     var buffer = new StringBuffer();
-
+    
     buffer.write("Sale {");
     buffer.write("id=" + "$id" + ", ");
     buffer.write("title=" + "$_title" + ", ");
@@ -148,66 +123,43 @@ class Sale extends Model {
     buffer.write("condition=" + "$_condition" + ", ");
     buffer.write("zipcode=" + "$_zipcode" + ", ");
     buffer.write("price=" + "$_price" + ", ");
-    buffer.write("createdAt=" +
-        (_createdAt != null ? _createdAt!.format() : "null") +
-        ", ");
-    buffer.write(
-        "updatedAt=" + (_updatedAt != null ? _updatedAt!.format() : "null"));
+    buffer.write("createdAt=" + (_createdAt != null ? _createdAt!.format() : "null") + ", ");
+    buffer.write("updatedAt=" + (_updatedAt != null ? _updatedAt!.format() : "null"));
     buffer.write("}");
-
+    
     return buffer.toString();
   }
-
-  Sale copyWith(
-      {String? id,
-      String? title,
-      String? description,
-      String? condition,
-      String? zipcode,
-      String? price,
-      List<SaleImage>? SaleImages}) {
+  
+  Sale copyWith({String? id, String? title, String? description, String? condition, String? zipcode, String? price, List<SaleImage>? SaleImages}) {
     return Sale._internal(
-        id: id ?? this.id,
-        title: title ?? this.title,
-        description: description ?? this.description,
-        condition: condition ?? this.condition,
-        zipcode: zipcode ?? this.zipcode,
-        price: price ?? this.price,
-        SaleImages: SaleImages ?? this.SaleImages);
+      id: id ?? this.id,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      condition: condition ?? this.condition,
+      zipcode: zipcode ?? this.zipcode,
+      price: price ?? this.price,
+      SaleImages: SaleImages ?? this.SaleImages);
   }
-
-  Sale.fromJson(Map<String, dynamic> json)
-      : id = json['id'],
-        _title = json['title'],
-        _description = json['description'],
-        _condition = json['condition'],
-        _zipcode = json['zipcode'],
-        _price = json['price'],
-        _SaleImages = json['SaleImages'] is List
-            ? (json['SaleImages'] as List)
-                .where((e) => e?['serializedData'] != null)
-                .map((e) => SaleImage.fromJson(
-                    new Map<String, dynamic>.from(e['serializedData'])))
-                .toList()
-            : null,
-        _createdAt = json['createdAt'] != null
-            ? TemporalDateTime.fromString(json['createdAt'])
-            : null,
-        _updatedAt = json['updatedAt'] != null
-            ? TemporalDateTime.fromString(json['updatedAt'])
-            : null;
-
+  
+  Sale.fromJson(Map<String, dynamic> json)  
+    : id = json['id'],
+      _title = json['title'],
+      _description = json['description'],
+      _condition = json['condition'],
+      _zipcode = json['zipcode'],
+      _price = json['price'],
+      _SaleImages = json['SaleImages'] is List
+        ? (json['SaleImages'] as List)
+          .where((e) => e?['serializedData'] != null)
+          .map((e) => SaleImage.fromJson(new Map<String, dynamic>.from(e['serializedData'])))
+          .toList()
+        : null,
+      _createdAt = json['createdAt'] != null ? TemporalDateTime.fromString(json['createdAt']) : null,
+      _updatedAt = json['updatedAt'] != null ? TemporalDateTime.fromString(json['updatedAt']) : null;
+  
   Map<String, dynamic> toJson() => {
-        'id': id,
-        'title': _title,
-        'description': _description,
-        'condition': _condition,
-        'zipcode': _zipcode,
-        'price': _price,
-        'SaleImages': _SaleImages?.map((SaleImage? e) => e?.toJson()).toList(),
-        'createdAt': _createdAt?.format(),
-        'updatedAt': _updatedAt?.format()
-      };
+    'id': id, 'title': _title, 'description': _description, 'condition': _condition, 'zipcode': _zipcode, 'price': _price, 'SaleImages': _SaleImages?.map((SaleImage? e) => e?.toJson()).toList(), 'createdAt': _createdAt?.format(), 'updatedAt': _updatedAt?.format()
+  };
 
   static final QueryField ID = QueryField(fieldName: "sale.id");
   static final QueryField TITLE = QueryField(fieldName: "title");
@@ -216,73 +168,81 @@ class Sale extends Model {
   static final QueryField ZIPCODE = QueryField(fieldName: "zipcode");
   static final QueryField PRICE = QueryField(fieldName: "price");
   static final QueryField SALEIMAGES = QueryField(
-      fieldName: "SaleImages",
-      fieldType: ModelFieldType(ModelFieldTypeEnum.model,
-          ofModelName: (SaleImage).toString()));
-  static var schema =
-      Model.defineSchema(define: (ModelSchemaDefinition modelSchemaDefinition) {
+    fieldName: "SaleImages",
+    fieldType: ModelFieldType(ModelFieldTypeEnum.model, ofModelName: (SaleImage).toString()));
+  static var schema = Model.defineSchema(define: (ModelSchemaDefinition modelSchemaDefinition) {
     modelSchemaDefinition.name = "Sale";
     modelSchemaDefinition.pluralName = "Sales";
-
+    
     modelSchemaDefinition.authRules = [
-      AuthRule(authStrategy: AuthStrategy.PUBLIC, operations: [
-        ModelOperation.CREATE,
-        ModelOperation.UPDATE,
-        ModelOperation.DELETE,
-        ModelOperation.READ
-      ])
+      AuthRule(
+        authStrategy: AuthStrategy.PUBLIC,
+        operations: [
+          ModelOperation.CREATE,
+          ModelOperation.UPDATE,
+          ModelOperation.DELETE,
+          ModelOperation.READ
+        ])
     ];
-
+    
     modelSchemaDefinition.addField(ModelFieldDefinition.id());
-
+    
     modelSchemaDefinition.addField(ModelFieldDefinition.field(
-        key: Sale.TITLE,
-        isRequired: false,
-        ofType: ModelFieldType(ModelFieldTypeEnum.string)));
-
+      key: Sale.TITLE,
+      isRequired: false,
+      ofType: ModelFieldType(ModelFieldTypeEnum.string)
+    ));
+    
     modelSchemaDefinition.addField(ModelFieldDefinition.field(
-        key: Sale.DESCRIPTION,
-        isRequired: false,
-        ofType: ModelFieldType(ModelFieldTypeEnum.string)));
-
+      key: Sale.DESCRIPTION,
+      isRequired: false,
+      ofType: ModelFieldType(ModelFieldTypeEnum.string)
+    ));
+    
     modelSchemaDefinition.addField(ModelFieldDefinition.field(
-        key: Sale.CONDITION,
-        isRequired: false,
-        ofType: ModelFieldType(ModelFieldTypeEnum.string)));
-
+      key: Sale.CONDITION,
+      isRequired: false,
+      ofType: ModelFieldType(ModelFieldTypeEnum.string)
+    ));
+    
     modelSchemaDefinition.addField(ModelFieldDefinition.field(
-        key: Sale.ZIPCODE,
-        isRequired: false,
-        ofType: ModelFieldType(ModelFieldTypeEnum.string)));
-
+      key: Sale.ZIPCODE,
+      isRequired: false,
+      ofType: ModelFieldType(ModelFieldTypeEnum.string)
+    ));
+    
     modelSchemaDefinition.addField(ModelFieldDefinition.field(
-        key: Sale.PRICE,
-        isRequired: false,
-        ofType: ModelFieldType(ModelFieldTypeEnum.string)));
-
+      key: Sale.PRICE,
+      isRequired: false,
+      ofType: ModelFieldType(ModelFieldTypeEnum.string)
+    ));
+    
     modelSchemaDefinition.addField(ModelFieldDefinition.hasMany(
-        key: Sale.SALEIMAGES,
-        isRequired: false,
-        ofModelName: (SaleImage).toString(),
-        associatedKey: SaleImage.SALEID));
-
+      key: Sale.SALEIMAGES,
+      isRequired: false,
+      ofModelName: (SaleImage).toString(),
+      associatedKey: SaleImage.SALEID
+    ));
+    
     modelSchemaDefinition.addField(ModelFieldDefinition.nonQueryField(
-        fieldName: 'createdAt',
-        isRequired: false,
-        isReadOnly: true,
-        ofType: ModelFieldType(ModelFieldTypeEnum.dateTime)));
-
+      fieldName: 'createdAt',
+      isRequired: false,
+      isReadOnly: true,
+      ofType: ModelFieldType(ModelFieldTypeEnum.dateTime)
+    ));
+    
     modelSchemaDefinition.addField(ModelFieldDefinition.nonQueryField(
-        fieldName: 'updatedAt',
-        isRequired: false,
-        isReadOnly: true,
-        ofType: ModelFieldType(ModelFieldTypeEnum.dateTime)));
+      fieldName: 'updatedAt',
+      isRequired: false,
+      isReadOnly: true,
+      ofType: ModelFieldType(ModelFieldTypeEnum.dateTime)
+    ));
   });
 }
 
 class _SaleModelType extends ModelType<Sale> {
   const _SaleModelType();
-
+  
   @override
   Sale fromJson(Map<String, dynamic> jsonData) {
     return Sale.fromJson(jsonData);
