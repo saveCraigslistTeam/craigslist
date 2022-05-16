f4afb3fe0a552835b7e38bd94538b70d357f8d6b
cc-smith <chriscolinsmith@gmail.com>
2022-05-16 16:29:49 -0600

    changed to expanding cards

--- lib/views/sales/my_sales.dart
+++ lib/views/sales/my_sales.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:core';
 import 'package:craigslist/views/sales/edit_sale.dart';
+import 'package:craigslist/views/sales/services/convert_date.dart';
 import 'package:intl/intl.dart';
 
 // flutter and ui libraries
@@ -15,6 +16,8 @@ import 'package:image_picker/image_picker.dart';
 import '../../models/ModelProvider.dart';
 import 'add_sale.dart';
 import 'services/fetch_image.dart';
+import 'package:expansion_tile_card/expansion_tile_card.dart';
+import 'package:expandable/expandable.dart';
 
 final oCcy = NumberFormat("#,##0", "en_US");
 
@@ -109,10 +112,11 @@ class SaleItem extends StatefulWidget {
   SaleItem({required this.sale});
 
   @override
-  State<SaleItem> createState() => _SaleItemState();
+  SaleItemState createState() => SaleItemState();
 }
 
-class _SaleItemState extends State<SaleItem> {
+class SaleItemState extends State<SaleItem> {
+  final GlobalKey<ExpansionTileCardState> cardA = new GlobalKey();
   late List<SaleImage> saleImages;
   late StreamSubscription<QuerySnapshot<SaleImage>> _subscription;
   bool _isLoading = true;
@@ -131,69 +135,336 @@ class _SaleItemState extends State<SaleItem> {
     if (_isLoading) {
       getImageStream();
     }
-    return InkWell(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => EditSaleForm(
-                      sale: widget.sale,
-                      saleImages: saleImages,
-                    )),
-          );
-        },
-        child: Card(
-            shadowColor: Theme.of(context).shadowColor,
-            elevation: 6,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            margin: EdgeInsets.only(left: 15, right: 15, top: 7.5, bottom: 7.5),
-            child: Column(
-              children: [
-                ListTile(
-                  dense: true,
-                  tileColor: Theme.of(context).cardColor,
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.vertical(
-                      top: Radius.circular(10),
-                    ),
-                  ),
-                  title: Text(
-                    heading!,
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
-                  ),
-                  subtitle: Text(
-                    subheading,
-                    style: const TextStyle(
+    return ExpandableNotifier(
+        child: Padding(
+      padding: const EdgeInsets.all(10),
+      child: Card(
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          children: <Widget>[
+            cardImage,
+            ScrollOnExpand(
+              scrollOnExpand: true,
+              scrollOnCollapse: false,
+              child: ExpandablePanel(
+                theme: const ExpandableThemeData(
+                  headerAlignment: ExpandablePanelHeaderAlignment.center,
+                  tapBodyToCollapse: true,
+                ),
+                header: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      heading!,
+                      style: const TextStyle(
+                        color: Colors.black,
                         fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                        color: Colors.green),
-                  ),
-                  trailing: IconButton(
-                      onPressed: () => showAlert(context),
-                      icon: Icon(Icons.delete_rounded)),
-                ),
-                SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.3,
-                    child: cardImage),
-                Container(
-                  padding: EdgeInsets.all(16.0),
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    supportingText!,
-                    style: const TextStyle(
-                      fontSize: 16,
+                        fontSize: 20,
+                      ),
+                      softWrap: true,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    )),
+                collapsed: Row(
+                  children: [
+                    Text(
+                      subheading,
+                      style: const TextStyle(
+                        color: Colors.green,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                      softWrap: true,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
                     ),
-                  ),
+                    Spacer(),
+                    Text(
+                      widget.sale.user!,
+                      style: const TextStyle(
+                        color: Colors.blueGrey,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                      softWrap: true,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
                 ),
-              ],
-            )));
+                expanded: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(children: [
+                      Text(
+                        subheading,
+                        style: const TextStyle(
+                          color: Colors.green,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                        softWrap: true,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Spacer(),
+                      Text(
+                        widget.sale.user!,
+                        style: const TextStyle(
+                          color: Colors.blueGrey,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                        softWrap: true,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ]),
+                    Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Text('Condition: ${widget.sale.condition}'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Text('${widget.sale.description}'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Text('${convertDate(widget.sale.updatedAt)}'),
+                    ),
+                    ElevatedButton.icon(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/msgDetail',
+                              arguments: [
+                                'username',
+                                widget.sale.id,
+                                widget.sale.user
+                              ]);
+                        },
+                        style: ElevatedButton.styleFrom(
+                            primary: Color(0xffA682FF)),
+                        icon: Icon(Icons.message, size: 18),
+                        label: Text('Message ${widget.sale.user}'))
+                  ],
+                ),
+                builder: (_, collapsed, expanded) {
+                  return Padding(
+                    padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                    child: Expandable(
+                      collapsed: collapsed,
+                      expanded: expanded,
+                      theme: const ExpandableThemeData(crossFadePoint: 0),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
   }
+
+// class SaleItem extends StatefulWidget {
+//   final Sale sale;
+//   SaleItem({required this.sale});
+
+//   @override
+//   SaleItemState createState() => SaleItemState();
+// }
+
+// class SaleItemState extends State<SaleItem> {
+//   final GlobalKey<ExpansionTileCardState> cardA = new GlobalKey();
+//   late List<SaleImage> saleImages;
+//   late StreamSubscription<QuerySnapshot<SaleImage>> _subscription;
+//   bool _isLoading = true;
+//   @override
+//   void initState() {
+//     saleImages = [];
+//     super.initState();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     var heading = widget.sale.title;
+//     var subheading = '\$${oCcy.format(widget.sale.price!)}';
+//     var cardImage = fetchImage(saleImages);
+//     var supportingText = widget.sale.description;
+//     if (_isLoading) {
+//       getImageStream();
+//     }
+//     return ExpansionTileCard(
+//       baseColor: Colors.white,
+//       expandedColor: Theme.of(context).selectedRowColor,
+//       key: cardA,
+//       title: Text(heading!),
+//       subtitle: Text(subheading),
+//       leading: SizedBox(
+//         height: 100.0,
+//         width: 100.0,
+//         child: cardImage,
+//       ),
+//       children: [
+//         SizedBox(
+//             height: MediaQuery.of(context).size.height * 0.3, child: cardImage),
+//         Align(
+//           alignment: Alignment.centerLeft,
+//           child: Padding(
+//             padding: const EdgeInsets.symmetric(
+//               horizontal: 16.0,
+//               vertical: 8.0,
+//             ),
+//             child: Text(supportingText!,
+//                 style: Theme.of(context).textTheme.bodyText2),
+//           ),
+//         ),
+//         ButtonBar(
+//           alignment: MainAxisAlignment.spaceAround,
+//           buttonHeight: 52.0,
+//           buttonMinWidth: 90.0,
+//           children: <Widget>[
+//             FlatButton(
+//               shape: RoundedRectangleBorder(
+//                   borderRadius: BorderRadius.circular(4.0)),
+//               onPressed: () {
+//                 cardA.currentState?.expand();
+//               },
+//               child: Column(
+//                 children: <Widget>[
+//                   Icon(Icons.arrow_downward),
+//                   Padding(
+//                     padding: const EdgeInsets.symmetric(vertical: 2.0),
+//                   ),
+//                   Text('Open'),
+//                 ],
+//               ),
+//             ),
+//             FlatButton(
+//               shape: RoundedRectangleBorder(
+//                   borderRadius: BorderRadius.circular(4.0)),
+//               onPressed: () {
+//                 cardA.currentState?.collapse();
+//               },
+//               child: Column(
+//                 children: <Widget>[
+//                   Icon(Icons.arrow_upward),
+//                   Padding(
+//                     padding: const EdgeInsets.symmetric(vertical: 2.0),
+//                   ),
+//                   Text('Close'),
+//                 ],
+//               ),
+//             ),
+//             FlatButton(
+//               shape: RoundedRectangleBorder(
+//                   borderRadius: BorderRadius.circular(4.0)),
+//               onPressed: () {},
+//               child: Column(
+//                 children: <Widget>[
+//                   Icon(Icons.swap_vert),
+//                   Padding(
+//                     padding: const EdgeInsets.symmetric(vertical: 2.0),
+//                   ),
+//                   Text('Toggle'),
+//                 ],
+//               ),
+//             ),
+//           ],
+//         ),
+//       ],
+//     );
+//   }
+// }
+
+// class SaleItem extends StatefulWidget {
+//   final Sale sale;
+//   SaleItem({required this.sale});
+
+//   @override
+//   State<SaleItem> createState() => _SaleItemState();
+// }
+
+// class _SaleItemState extends State<SaleItem> {
+//   late List<SaleImage> saleImages;
+//   late StreamSubscription<QuerySnapshot<SaleImage>> _subscription;
+//   bool _isLoading = true;
+//   @override
+//   void initState() {
+//     saleImages = [];
+//     super.initState();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     var heading = widget.sale.title;
+//     var subheading = '\$${oCcy.format(widget.sale.price!)}';
+//     var cardImage = fetchImage(saleImages);
+//     var supportingText = widget.sale.description;
+//     if (_isLoading) {
+//       getImageStream();
+//     }
+//     return InkWell(
+//         onTap: () {
+//           Navigator.push(
+//             context,
+//             MaterialPageRoute(
+//                 builder: (context) => EditSaleForm(
+//                       sale: widget.sale,
+//                       saleImages: saleImages,
+//                     )),
+//           );
+//         },
+//         child: Card(
+//             shadowColor: Theme.of(context).shadowColor,
+//             elevation: 6,
+//             shape: RoundedRectangleBorder(
+//               borderRadius: BorderRadius.circular(10.0),
+//             ),
+//             margin: EdgeInsets.only(left: 15, right: 15, top: 7.5, bottom: 7.5),
+//             child: Column(
+//               children: [
+//                 ListTile(
+//                   dense: true,
+//                   tileColor: Theme.of(context).cardColor,
+//                   shape: const RoundedRectangleBorder(
+//                     borderRadius: BorderRadius.vertical(
+//                       top: Radius.circular(10),
+//                     ),
+//                   ),
+//                   title: Text(
+//                     heading!,
+//                     style: const TextStyle(
+//                       color: Colors.black,
+//                       fontWeight: FontWeight.bold,
+//                       fontSize: 20,
+//                     ),
+//                   ),
+//                   subtitle: Text(
+//                     subheading,
+//                     style: const TextStyle(
+//                         fontWeight: FontWeight.bold,
+//                         fontSize: 16,
+//                         color: Colors.green),
+//                   ),
+//                   trailing: IconButton(
+//                       onPressed: () => showAlert(context),
+//                       icon: Icon(Icons.delete_rounded)),
+//                 ),
+//                 SizedBox(
+//                     height: MediaQuery.of(context).size.height * 0.3,
+//                     child: cardImage),
+//                 Container(
+//                   padding: EdgeInsets.all(16.0),
+//                   alignment: Alignment.centerLeft,
+//                   child: Text(
+//                     supportingText!,
+//                     style: const TextStyle(
+//                       fontSize: 16,
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             )));
+  // }
 
   showAlert(BuildContext context) {
     // set up the buttons
